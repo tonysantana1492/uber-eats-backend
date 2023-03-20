f106919faef6585b946b8c5f1cbfee3f94fed3d9
Tony Santana Lopez <tonysantana1492@gmail.com>
2023-03-20 07:40:24 -0400

    feat(jwt): create jwt module

---
+++ src/jwt/jwt.module.ts
@@ -0,0 +1,22 @@
+import { DynamicModule, Module, Global } from '@nestjs/common';
+import { JwtService } from './jwt.service';
+import { CONFIG_OPTIONS } from './jwt.constants';
+import { JwtModuleOptions } from './jwt.interfaces';
+
+@Module({})
+@Global()
+export class JwtModule {
+	static forRoot(options: JwtModuleOptions): DynamicModule {
+		return {
+			module: JwtModule,
+			exports: [JwtService],
+			providers: [
+				{
+					provide: CONFIG_OPTIONS,
+					useValue: options,
+				},
+				JwtService,
+			],
+		};
+	}
+}
