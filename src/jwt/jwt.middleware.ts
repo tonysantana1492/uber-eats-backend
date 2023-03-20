da020db7d4f3e28c7f00d39b49f83449fd0e3fbf
Tony Santana Lopez <tonysantana1492@gmail.com>
2023-03-20 07:36:46 -0400

    feat(jwt): create jwt middleware for obtain user's information from the token

---
+++ src/jwt/jwt.middleware.ts
@@ -0,0 +1,26 @@
+import { NestMiddleware, Injectable } from '@nestjs/common';
+import { NextFunction, Request, Response } from 'express';
+import { JwtService } from './jwt.service';
+import { UsersService } from '../users/users.service';
+
+@Injectable()
+export class JwtMiddleware implements NestMiddleware {
+	constructor(private readonly jwtService: JwtService, private readonly usersService: UsersService) {}
+	async use(req: Request, res: Response, next: NextFunction) {
+		if ('x-jwt' in req.headers) {
+			const token = req.headers['x-jwt'];
+
+			try {
+				const decoded = this.jwtService.verify(token.toString());
+
+				if (typeof decoded === 'object' && decoded.hasOwnProperty('id')) {
+					const id = decoded['id'];
+					const user = await this.usersService.findById(id);
+
+					req['user'] = user;
+				}
+			} catch (error) {}
+		}
+		next();
+	}
+}
