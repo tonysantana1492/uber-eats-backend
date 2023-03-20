522bd0a7e24cb742bca06c4e89e4dd135f321a7d
Tony Santana Lopez <tonysantana1492@gmail.com>
2023-03-20 07:48:10 -0400

    feat(users): create CreateAccountInput and CreateAccountOutput dtos

---
+++ src/users/dtos/create-account.dto.ts
@@ -0,0 +1,9 @@
+import { PickType, ObjectType, InputType } from '@nestjs/graphql';
+import { User } from '../entities/user.entity';
+import { CoreOutput } from '../../common/dtos/output.dto';
+
+@InputType()
+export class CreateAccountInput extends PickType(User, ['email', 'password', 'role']) {}
+
+@ObjectType()
+export class CreateAccountOutput extends CoreOutput {}
