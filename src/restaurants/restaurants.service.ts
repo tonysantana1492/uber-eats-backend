fb815468c23e60ccf8c092474fc9b1dcb1a1143a
Tony Santana Lopez <tonysantana1492@gmail.com>
2023-03-20 07:41:42 -0400

    feat(restaurants): create restaurants service

---
+++ src/restaurants/restaurants.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { Restaurant } from './entities/restaurant.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
+import { UpdateRestaurantDto } from './dtos/update-restaurant.dto';
+
+@Injectable()
+export class RestaurantsService {
+	constructor(@InjectRepository(Restaurant) private readonly restaurants: Repository<Restaurant>) {}
+
+	getAll(): Promise<Restaurant[]> {
+		return this.restaurants.find();
+	}
+
+	createRestaurant(createRestaurantDto: CreateRestaurantDto): Promise<Restaurant> {
+		const newRestaurant = this.restaurants.create(createRestaurantDto);
+		return this.restaurants.save(newRestaurant);
+	}
+
+	async updateRestaurant({ id, data }: UpdateRestaurantDto) {
+		return this.restaurants.update(id, { ...data });
+	}
+}
