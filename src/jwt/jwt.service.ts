682cd92414b2862bc40a9437488a536c233e1bf7
Tony Santana Lopez <tonysantana1492@gmail.com>
2023-03-26 22:06:35 -0400

    refactor(): remove hello method

--- src/jwt/jwt.service.ts
+++ src/jwt/jwt.service.ts
@@ -1,15 +1,11 @@
 import { Injectable, Inject } from '@nestjs/common';
 import { JwtModuleOptions } from './jwt.interfaces';
-import { CONFIG_OPTIONS } from './jwt.constants';
+import { CONFIG_OPTIONS } from '../common/common.constants';
 import * as jwt from 'jsonwebtoken';
 
 @Injectable()
 export class JwtService {
 	constructor(@Inject(CONFIG_OPTIONS) private readonly options: JwtModuleOptions) {}
-
-	hello() {
-		console.log('hello');
-	}
 
 	sign(userId: number): string {
 		return jwt.sign({ id: userId }, this.options.privateKey);
