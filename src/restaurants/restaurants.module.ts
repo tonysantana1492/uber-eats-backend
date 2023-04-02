61d8465c28d6fe3c5af7cf8544d595489e55f277
Tony Santana Lopez <tonysantana1492@gmail.com>
2023-04-01 22:10:10 -0400

    refactor(restaurants): add CategoryRepository created to providers and  TypeOrmModule imports

--- src/restaurants/restaurants.module.ts
+++ src/restaurants/restaurants.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { RestaurantResolver } from './restaurants.resolver';
 import { RestaurantsService } from './restaurants.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CategoryRepository } from './repositories/category.repository';
 import { Restaurant } from './entities/restaurant.entity';
 
 @Module({
-	imports: [TypeOrmModule.forFeature([Restaurant])],
-	providers: [RestaurantResolver, RestaurantsService],
+	imports: [TypeOrmModule.forFeature([Restaurant, CategoryRepository])],
+	providers: [RestaurantResolver, RestaurantsService, CategoryRepository],
 })
 export class RestaurantsModule {}
