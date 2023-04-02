803725c0b7c6f22ec5082a5f712f9d2e1f90560d
Tony Santana Lopez <tonysantana1492@gmail.com>
2023-04-01 21:26:30 -0400

    feat(restaurants): create CreateRestaurantInput and CreateRestaurantOutput dtos

--- src/restaurants/dtos/create-restaurant.dto.ts
+++ src/restaurants/dtos/create-restaurant.dto.ts
@@ -1,7 +1,17 @@
-import { InputType, OmitType } from '@nestjs/graphql';
+import { InputType, ObjectType, PickType, Field, Int } from '@nestjs/graphql';
 
 import { Restaurant } from '../entities/restaurant.entity';
+import { CoreOutput } from '../../common/dtos/output.dto';
 
-@InputType()
 // Puedo pasar InputType como argumento para cambiar el decorador del padre que es ObjectType por este InputType
-export class CreateRestaurantDto extends OmitType(Restaurant, ['id']) {}
+@InputType()
+export class CreateRestaurantInput extends PickType(Restaurant, ['name', 'address', 'coverImg']) {
+	@Field(() => String)
+	categoryName: string;
+}
+
+@ObjectType()
+export class CreateRestaurantOutput extends CoreOutput {
+	@Field(() => Int)
+	restaurantId?: number;
+}
