c553a021af37157a716162a987510e226e992db7
Tony Santana Lopez <tonysantana1492@gmail.com>
2023-04-01 22:08:18 -0400

    chore(restaurants): add delete restaurant logic

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,35 +1,41 @@
-import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
-import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
-import { UpdateRestaurantDto } from './dtos/update-restaurant.dto';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { AuthUser } from 'src/auth/auth-user.decorator';
+import { CreateRestaurantInput, CreateRestaurantOutput } from './dtos/create-restaurant.dto';
 import { Restaurant } from './entities/restaurant.entity';
 import { RestaurantsService } from './restaurants.service';
+import { User, UserRole } from '../users/entities/user.entity';
+import { Role } from '../auth/role.decorator';
+import { EditRestaurantOutput, EditRestaurantInput } from './dtos/edit-restaurant.dto';
+import { DeleteRestaurantOutput, DeleteRestaurantInput } from './dtos/delete-restaurant.dto';
 
 @Resolver(() => Restaurant)
 export class RestaurantResolver {
 	constructor(private readonly restaurantsService: RestaurantsService) {}
 
-	@Query(() => [Restaurant])
-	restaurants(): Promise<Restaurant[]> {
-		return this.restaurantsService.getAll();
+	@Mutation(() => CreateRestaurantOutput)
+	@Role([UserRole.Owner])
+	createRestaurant(
+		@AuthUser() authUser: User,
+		@Args('input') createRestaurantInput: CreateRestaurantInput,
+	): Promise<CreateRestaurantOutput> {
+		return this.restaurantsService.createRestaurant(authUser, createRestaurantInput);
 	}
 
-	@Mutation(() => Boolean)
-	async createRestaurant(@Args('input') createRestaurantDto: CreateRestaurantDto): Promise<boolean> {
-		try {
-			await this.restaurantsService.createRestaurant(createRestaurantDto);
-			return true;
-		} catch (error) {
-			return false;
-		}
+	@Mutation(() => EditRestaurantOutput)
+	@Role([UserRole.Owner])
+	editRestaurant(
+		@AuthUser() owner: User,
+		@Args('input') editRestaurantInputType: EditRestaurantInput,
+	): Promise<EditRestaurantOutput> {
+		return this.restaurantsService.editRestaurant(owner, editRestaurantInputType);
 	}
 
-	@Mutation(() => Boolean)
-	async updateRestaurant(@Args('input') updateRestaurantDto: UpdateRestaurantDto): Promise<boolean> {
-		try {
-			await this.restaurantsService.updateRestaurant(updateRestaurantDto);
-			return true;
-		} catch (error) {
-			return false;
-		}
+	@Mutation(() => DeleteRestaurantOutput)
+	@Role([UserRole.Owner])
+	deleteRestaurant(
+		@AuthUser() owner: User,
+		@Args('input') deleteRestaurantInput: DeleteRestaurantInput,
+	): Promise<DeleteRestaurantOutput> {
+		return this.restaurantsService.deleteRestaurant(owner, deleteRestaurantInput);
 	}
 }
