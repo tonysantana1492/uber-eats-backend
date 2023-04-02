3c5707a5a6ab1f79a2b9e7a013134358c44af789
Tony Santana Lopez <tonysantana1492@gmail.com>
2023-04-01 22:07:30 -0400

    chore(restaurants): add delete restaurant  logic

--- src/restaurants/restaurants.service.ts
+++ src/restaurants/restaurants.service.ts
@@ -1,24 +1,86 @@
 import { Injectable } from '@nestjs/common';
+import { CreateRestaurantInput, CreateRestaurantOutput } from './dtos/create-restaurant.dto';
+import { User } from '../users/entities/user.entity';
+import { EditRestaurantOutput, EditRestaurantInput } from './dtos/edit-restaurant.dto';
+import { CategoryRepository } from './repositories/category.repository';
+import { Repository } from 'typeorm';
 import { Restaurant } from './entities/restaurant.entity';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
-import { UpdateRestaurantDto } from './dtos/update-restaurant.dto';
+import { Category } from './entities/category.entity';
+import { DeleteRestaurantOutput, DeleteRestaurantInput } from './dtos/delete-restaurant.dto';
 
 @Injectable()
 export class RestaurantsService {
-	constructor(@InjectRepository(Restaurant) private readonly restaurants: Repository<Restaurant>) {}
+	constructor(
+		private readonly categories: CategoryRepository,
+		@InjectRepository(Restaurant)
+		private readonly restaurants: Repository<Restaurant>,
+	) {}
 
-	getAll(): Promise<Restaurant[]> {
-		return this.restaurants.find();
+	async createRestaurant(owner: User, createRestaurantInput: CreateRestaurantInput): Promise<CreateRestaurantOutput> {
+		try {
+			const newRestaurant = this.restaurants.create(createRestaurantInput);
+			newRestaurant.owner = owner;
+
+			const category = await this.categories.getOrCreate(createRestaurantInput.categoryName);
+
+			newRestaurant.category = category;
+			await this.restaurants.save(newRestaurant);
+
+			return { ok: true };
+		} catch (error) {
+			return { ok: false, error: 'Could not create restaurant' };
+		}
 	}
 
-	createRestaurant(createRestaurantDto: CreateRestaurantDto): Promise<Restaurant> {
-		const newRestaurant = this.restaurants.create(createRestaurantDto);
-		return this.restaurants.save(newRestaurant);
+	async editRestaurant(owner: User, editRestaurantInput: EditRestaurantInput): Promise<EditRestaurantOutput> {
+		try {
+			const restaurant = await this.restaurants.findOneBy({ id: editRestaurantInput.restaurantId });
+
+			if (!restaurant) {
+				return { ok: false, error: 'Resturant not found' };
+			}
+
+			if (owner.id !== restaurant.ownerId) {
+				return { ok: false, error: "You can't edit a restaurant that you don't own" };
+			}
+
+			let category: Category = null;
+			if (editRestaurantInput.categoryName) {
+				category = await this.categories.getOrCreate(editRestaurantInput.categoryName);
+			}
+
+			const editedRestaurant = {
+				id: editRestaurantInput.restaurantId,
+				...editRestaurantInput,
+				...(category && { category }),
+			};
+
+			await this.restaurants.save([editedRestaurant]);
+
+			return { ok: true };
+		} catch (error) {
+			return { ok: false, error };
+		}
 	}
 
-	async updateRestaurant({ id, data }: UpdateRestaurantDto) {
-		return this.restaurants.update(id, { ...data });
+	async deleteRestaurant(owner: User, { restaurantId }: DeleteRestaurantInput): Promise<DeleteRestaurantOutput> {
+		try {
+			const restaurant = await this.restaurants.findOneBy({ id: restaurantId });
+
+			if (!restaurant) {
+				return { ok: false, error: 'Restauran not found' };
+			}
+
+			if (restaurant.ownerId !== owner.id) {
+				return { ok: false, error: "You can't delete a restaurant that don't own" };
+			}
+
+			await this.restaurants.delete(restaurantId);
+
+			return { ok: true };
+		} catch (error) {
+			return { ok: false, error: 'Could not delete restaurant' };
+		}
 	}
 }
