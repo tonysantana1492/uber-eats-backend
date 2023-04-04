7cf2646ce96042990d177e45023953bdfcd1a09d
Tony Santana Lopez <tonysantana1492@gmail.com>
2023-04-03 20:36:57 -0400

    feat(common): create PaginationInput and PaginationOutput dtos

---
+++ src/common/dtos/pagination.dto.ts
@@ -0,0 +1,17 @@
+import { CoreOutput } from './output.dto';
+import { ObjectType, InputType, Field } from '@nestjs/graphql';
+
+@InputType()
+export class PaginationInput {
+	@Field(() => Number, { defaultValue: 1 })
+	page: number;
+}
+
+@ObjectType()
+export class PaginationOutput extends CoreOutput {
+	@Field(() => Number, { nullable: true })
+	totalPages?: number;
+
+	@Field(() => Number, { nullable: true })
+	totalResult?: number;
+}
