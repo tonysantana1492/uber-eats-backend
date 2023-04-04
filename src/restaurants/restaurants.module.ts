a3b8e32aeff3dd90a76629811ae6c3e088775ac6
Tony Santana Lopez <tonysantana1492@gmail.com>
2023-04-03 20:45:45 -0400

    feat(restaurants): add CategoryService and CategoryResolver to providers

--- src/restaurants/restaurants.module.ts
+++ src/restaurants/restaurants.module.ts
@@ -4,9 +4,11 @@ import { RestaurantsService } from './restaurants.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CategoryRepository } from './repositories/category.repository';
 import { Restaurant } from './entities/restaurant.entity';
+import { CategoryService } from './category.service';
+import { CategoryResolver } from './category.resolver';
 
 @Module({
 	imports: [TypeOrmModule.forFeature([Restaurant, CategoryRepository])],
-	providers: [RestaurantResolver, RestaurantsService, CategoryRepository],
+	providers: [RestaurantResolver, RestaurantsService, CategoryRepository, CategoryResolver, CategoryService],
 })
 export class RestaurantsModule {}
