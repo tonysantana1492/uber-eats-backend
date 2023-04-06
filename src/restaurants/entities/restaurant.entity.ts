afb3e3bfc7bf0c3a9c5e5c03cf1006dde92fec47
Tony Santana Lopez <tonysantana1492@gmail.com>
2023-04-05 22:13:10 -0400

    feat(restaurants): add relation one to many between restaurant entity and dish entity for implement the menu of the restaurant

--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -1,12 +1,15 @@
 import { Field, InputType, ObjectType } from '@nestjs/graphql';
-import { Column, Entity, ManyToOne, RelationId } from 'typeorm';
+import { Column, Entity, ManyToOne, OneToMany, RelationId } from 'typeorm';
 import { IsString, Length } from 'class-validator';
 import { CoreEntity } from '../../common/entities/core.entity';
 import { Category } from './category.entity';
 import { User } from '../../users/entities/user.entity';
+import { Dish } from './dish.entity';
+import { Order } from 'src/orders/entities/order.entity';
 
-@InputType('RestaurantInputType', { isAbstract: true }) // Uso el InputType para que el DTO pueda extender de esta entidad
-@ObjectType() // Esto es para el esquema de GraphQL le dice como lucen los datos, generalment para la respuesta de los queries y las mutations
+// El isAbstract evita que se cree un type RestaurantInputType en el esquema
+@InputType('RestaurantInputType', { isAbstract: true }) // Uso el InputType para que un DTO pueda extender de esta entidad
+@ObjectType() // Esto es para el esquema de GraphQL le dice como lucen los datos, lo uso en los @Resolver
 @Entity() // Esto es para el TypeORM para crear la tabla
 export class Restaurant extends CoreEntity {
 	@Field(() => String)
@@ -36,4 +39,12 @@ export class Restaurant extends CoreEntity {
 	// Para que te cargue el id de usuario ya que por defecto no te lo da en la consulta aunque este en la tabla resturant
 	@RelationId((restaurant: Restaurant) => restaurant.owner)
 	ownerId: number;
+
+	@Field(() => [Dish], { nullable: true })
+	@OneToMany(() => Dish, dish => dish.restaurant)
+	menu: Dish[];
+
+	@Field(() => [Order], { nullable: true })
+	@OneToMany(() => Order, order => order.restaurant)
+	orders: Order[];
 }
