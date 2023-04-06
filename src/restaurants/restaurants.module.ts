eb990d405e01f4a45ab92b038be4c9b7e825ce34
Tony Santana Lopez <tonysantana1492@gmail.com>
2023-04-05 22:07:35 -0400

    feat(restaurants): add DishResolver and DishService to providers

--- src/restaurants/restaurants.module.ts
+++ src/restaurants/restaurants.module.ts
@@ -6,9 +6,20 @@ import { CategoryRepository } from './repositories/category.repository';
 import { Restaurant } from './entities/restaurant.entity';
 import { CategoryService } from './category.service';
 import { CategoryResolver } from './category.resolver';
+import { Dish } from './entities/dish.entity';
+import { DishResolver } from './dish.resolver';
+import { DishService } from './dish.service';
 
 @Module({
-	imports: [TypeOrmModule.forFeature([Restaurant, CategoryRepository])],
-	providers: [RestaurantResolver, RestaurantsService, CategoryRepository, CategoryResolver, CategoryService],
+	imports: [TypeOrmModule.forFeature([Restaurant, CategoryRepository, Dish])],
+	providers: [
+		RestaurantResolver,
+		RestaurantsService,
+		CategoryRepository,
+		CategoryResolver,
+		CategoryService,
+		DishResolver,
+		DishService,
+	],
 })
 export class RestaurantsModule {}
