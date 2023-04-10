1c96736aa7410da4cc6b99548ecbef4637a4c6a4
Tony Santana Lopez <tonysantana1492@gmail.com>
2023-04-10 16:46:05 -0400

    refactor(jwt): reemplace token key string by a constant

--- src/jwt/jwt.middleware.ts
+++ src/jwt/jwt.middleware.ts
@@ -2,13 +2,15 @@ import { NestMiddleware, Injectable } from '@nestjs/common';
 import { NextFunction, Request, Response } from 'express';
 import { JwtService } from './jwt.service';
 import { UsersService } from '../users/users.service';
+import { TOKEN_KEY } from 'src/common/common.constants';
 
 @Injectable()
+// ! No lo uso
 export class JwtMiddleware implements NestMiddleware {
 	constructor(private readonly jwtService: JwtService, private readonly usersService: UsersService) {}
 	async use(req: Request, res: Response, next: NextFunction) {
-		if ('x-jwt' in req.headers) {
-			const token = req.headers['x-jwt'];
+		if (TOKEN_KEY in req.headers) {
+			const token = req.headers[TOKEN_KEY];
 
 			try {
 				const decoded = this.jwtService.verify(token.toString());
