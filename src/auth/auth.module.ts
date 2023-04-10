3c044c9f1d02458c77b9a072f3717b5180e0ab3d
Tony Santana Lopez <tonysantana1492@gmail.com>
2023-04-10 16:40:18 -0400

    feat(auth): add UserModule to imports

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
 import { AuthGuard } from './auth.guard';
+import { UserModule } from 'src/users/users.module';
 
 @Module({
+	imports: [UserModule],
 	providers: [
 		{
-			provide: APP_GUARD,
+			provide: APP_GUARD, // Aplico AuthGuard a toda la aplicacion
 			useClass: AuthGuard,
 		},
 	],
