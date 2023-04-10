9dd01d5d20294edeebeb2c57cac063ffef31d164
Tony Santana Lopez <tonysantana1492@gmail.com>
2023-04-10 16:44:45 -0400

    test(users): fix role enum in the createAccountArgs

--- src/users/tests/users.service.spec.ts
+++ src/users/tests/users.service.spec.ts
@@ -1,7 +1,7 @@
 import { UsersService } from '../users.service';
 import { Test } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
-import { User } from '../entities/user.entity';
+import { User, UserRole } from '../entities/user.entity';
 import { Verification } from '../entities/verification.entity';
 import { JwtService } from '../../jwt/jwt.service';
 import { MailService } from '../../mail/mail.service';
@@ -77,7 +77,7 @@ describe('UserService', () => {
 		const createAccountArgs = {
 			email: '',
 			password: '',
-			role: 0,
+			role: UserRole.Client,
 		};
 
 		it('should fail if user exists', async () => {
