993d5aa0cf3ec4f1fc52a6ce667c6159c5c1d978
Tony Santana Lopez <tonysantana1492@gmail.com>
2023-04-10 16:38:41 -0400

    feature(common): add pubSub as a provider and stablish the module as global

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,1 +1,19 @@
+import { Global, Module } from '@nestjs/common';
+import { PUB_SUB } from './common.constants';
+import { PubSub } from 'graphql-subscriptions';
+
+// TODO: Si deseo tener varias instancias de mi servidor, esto no funcionará ya que pubSub debe ser unico para todas esas instancias
+// TODO: Tengo que crear entonces un pubsub implementation con redis.
+const pubSub = new PubSub();
+
+@Global()
+@Module({
+	providers: [
+		{
+			provide: PUB_SUB,
+			useValue: pubSub,
+		},
+	],
+	exports: [PUB_SUB],
+})
 export class CommonModule {}
