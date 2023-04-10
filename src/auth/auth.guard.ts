5b7da1fc89806567fdbf3c9bcac9a4a5b61f314c
Tony Santana Lopez <tonysantana1492@gmail.com>
2023-04-10 16:36:12 -0400

    refactor(): move token verification from jwt to AuthGuard for handler http and ws requests

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -1,26 +1,46 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { GqlExecutionContext } from '@nestjs/graphql';
-import { User } from '../users/entities/user.entity';
 import { AllowedRoles } from './role.decorator';
+import { JwtService } from 'src/jwt/jwt.service';
+import { UsersService } from 'src/users/users.service';
+import { TOKEN_KEY } from 'src/common/common.constants';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-	constructor(private readonly reflector: Reflector) {}
-	canActivate(context: ExecutionContext) {
+	constructor(
+		private readonly reflector: Reflector,
+		private readonly jwtService: JwtService,
+		private readonly usersService: UsersService,
+	) {}
+	async canActivate(context: ExecutionContext) {
 		const roles = this.reflector.get<AllowedRoles>('roles', context.getHandler());
-
-		// Es una ruta publica
-		if (!roles) return true;
+		if (!roles) {
+			return true;
+		}
 
 		const gqlContext = GqlExecutionContext.create(context).getContext();
-		const user: User = gqlContext['user'];
 
-		// Existe un usuario autenticado y su informacion se extrajo del token
-		if (!user) return false;
+		const token = gqlContext.headers[TOKEN_KEY];
 
-		if (roles.includes('Any')) return true;
+		if (!token) return false;
 
-		return roles.includes(user.role);
+		const decoded = this.jwtService.verify(token.toString());
+
+		if (typeof decoded === 'object' && decoded.hasOwnProperty('id')) {
+			const id = decoded['id'];
+			const { user } = await this.usersService.findById(id);
+
+			// Existe un usuario autenticado y su informacion se extrajo del token
+			if (!user) return false;
+
+			gqlContext.user = user;
+
+			if (roles.includes('Any')) return true;
+
+			return roles.includes(user.role);
+		} else {
+			return false;
+		}
 	}
 }
