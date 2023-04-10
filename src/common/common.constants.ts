8991552cd296d3aaf37b611ba14ed9d0ef1255eb
Tony Santana Lopez <tonysantana1492@gmail.com>
2023-04-10 16:48:22 -0400

    feat(common): create new constants for subscription graphql events

--- src/common/common.constants.ts
+++ src/common/common.constants.ts
@@ -1,1 +1,8 @@
 export const CONFIG_OPTIONS = 'CONFIG_OPTIONS';
+export const PUB_SUB = 'PUB_SUB';
+export const TOKEN_KEY = 'x-jwt';
+
+// Triguer Subscriptions
+export const NEW_PENDING_ORDER = 'NEW_PENDING_ORDER';
+export const NEW_COOKED_ORDER = 'NEW_COOKED_ORDER';
+export const NEW_ORDER_UPDATE = 'NEW_ORDER_UPDATE';
