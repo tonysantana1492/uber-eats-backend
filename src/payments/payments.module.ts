9465631ac9b5db260da91f124d7c12f2a9ae870b
Tony Santana Lopez <tonysantana1492@gmail.com>
2023-04-10 23:55:35 -0400

    feat(payments): create payments module

---
+++ src/payments/payments.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { PaymentsService } from './payments.service';
+import { PaymentsResolver } from './payments.resolver';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Payment } from './entities/payment.entity';
+import { Restaurant } from 'src/restaurants/entities/restaurant.entity';
+
+@Module({
+	imports: [TypeOrmModule.forFeature([Payment, Restaurant])],
+	providers: [PaymentsService, PaymentsResolver],
+})
+export class PaymentsModule {}
