21de1b95dfb49d671d22e5624d77e4de0ad85fb5
Tony Santana Lopez <tonysantana1492@gmail.com>
2023-05-13 20:24:02 -0400

    feat(payments): add PaymentsController to the module

--- src/payments/payments.module.ts
+++ src/payments/payments.module.ts
@@ -4,9 +4,11 @@ import { PaymentsResolver } from './payments.resolver';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Payment } from './entities/payment.entity';
 import { Restaurant } from 'src/restaurants/entities/restaurant.entity';
+import { PaymentsController } from './payments.controller';
 
 @Module({
 	imports: [TypeOrmModule.forFeature([Payment, Restaurant])],
+	controllers: [PaymentsController],
 	providers: [PaymentsService, PaymentsResolver],
 })
 export class PaymentsModule {}
